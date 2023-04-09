fce68cb0f73065c44015876c5bdd563d375e2d1e
LokLambert18 <you@example.com>
2023-04-09 14:41:13 -0400

    MongoDB

--- app.js
+++ app.js
@@ -14,7 +14,6 @@ HelloController(app)
 UserController(app)
 TuitsController(app);
 app.listen(4000)
-mongoose.connect('mongodb://127.0.0.1:27017/tuiter');
 
 const CONNECTION_STRING = process.env.DB_CONNECTION_STRING
  || 'mongodb://127.0.0.1:27017/tuiter'
